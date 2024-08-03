246618a184b6d190725a9151901f0556f9f94d4c
Jacob Howell <jhowell45@googlemail.com>
2024-08-03 21:43:58 +0100

    Updated to run specific animations when walking or not.

--- player.gd
+++ player.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 
-
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 600
 	move_and_slide()
+	
+	if (get_real_velocity().is_zero_approx()):
+		get_node("HappyBoo").play_idle_animation()
+	else:
+		get_node("HappyBoo").play_walk_animation()
