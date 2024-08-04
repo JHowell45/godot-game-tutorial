a8593df8208bea27c92d8c322785905bf77bbf7a
Jacob Howell <jhowell45@googlemail.com>
2024-08-04 21:59:40 +0100

    Aded global position check for the player.

--- mob.gd
+++ mob.gd
@@ -1,4 +1,8 @@
 extends CharacterBody2D
 
+@onready var player = get_node("/root/Game/Player")
+
 func _physics_process(delta):
-	pass
+	var distance = global_position.direction_to(player.global_position)
+	velocity = distance * 300
+	move_and_slide()
