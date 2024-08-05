3dea7a7d6d4e017c62aa29179f6a59733d389c2f
Jacob Howell <jhowell45@googlemail.com>
2024-08-06 00:36:36 +0100

    Adding signal for applying an action to the bullet when it collides with a body.

--- bullet.gd
+++ bullet.gd
@@ -1,6 +1,18 @@
 extends Area2D
 
+var distance_travelled = 0
 
 func _physics_process(delta):
+	const SPEED = 1000
+	const RANGE = 1200
 	var direction = Vector2.RIGHT.rotated(rotation)
-	position += direction * 1000 * delta
+	position += direction * SPEED * delta
+	distance_travelled += SPEED * delta
+	if distance_travelled > RANGE:
+		queue_free()
+
+
+func _on_body_entered(body):
+	queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage()
