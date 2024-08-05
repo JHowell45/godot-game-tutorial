2458a6a497df2efe239f3629ce5bf5da11a1aaeb
Jacob Howell <jhowell45@googlemail.com>
2024-08-06 00:16:14 +0100

    make the gun rotate to the first enemy in range.

--- gun.gd
+++ gun.gd
@@ -3,3 +3,6 @@ extends Area2D
 
 func _physics_process(delta):
 	var enemies_in_range = get_overlapping_bodies()
+	if enemies_in_range.size() > 0:
+		var target = enemies_in_range[0]
+		look_at(target.global_position)
