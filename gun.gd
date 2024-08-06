c78266858538b2de8758b0a7fffab7328826ad18
Jacob Howell <jhowell45@googlemail.com>
2024-08-06 23:13:33 +0100

    Updated hidden vars.

--- gun.gd
+++ gun.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var enemies_in_range = get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
 		var target = enemies_in_range[0]
