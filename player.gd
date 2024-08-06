97a6e4a97cee2226caf093feff4b7e9005edc32c
Jacob Howell <jhowell45@googlemail.com>
2024-08-06 22:49:28 +0100

    Updated player to have health and take damage.

--- player.gd
+++ player.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+var health = 100.0
+
+const DAMAGE_RATE = 5
+
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 600
@@ -9,3 +13,7 @@ func _physics_process(delta):
 		%HappyBoo.play_idle_animation()
 	else:
 		%HappyBoo.play_walk_animation()
+	
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	health -= overlapping_mobs * DAMAGE_RATE
+	
