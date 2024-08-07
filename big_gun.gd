582ef7196dc83b304addff55acd14ed91f5b96b4
Jacob Howell <jhowell45@googlemail.com>
2024-08-07 13:19:57 +0100

    basic physics impl.

--- big_gun.gd
+++ big_gun.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
 func _physics_process(delta):
-	pass
+	var enemies_in_range = get_overlapping_bodies()
+	if enemies_in_range.size() > 0:
+		var target = enemies_in_range[0]
+		look_at(target.global_position)
 
 func shoot():
 	const BULLET = preload("res://bullet.tscn")
@@ -12,6 +15,9 @@ func shoot():
 		BULLET.instantiate(),
 		BULLET.instantiate(),
 	]
+	for bullet: Node2D in bullets:
+		bullet.global_position = %ShootingPoint.global_position
+		%ShootingPoint.add_child(bullet)
 
 func _on_timer_timeout():
 	shoot()
