438c8744386562b1b96d67684cddec36195825b6
Jacob Howell <jhowell45@googlemail.com>
2024-08-07 13:04:04 +0100

    dynamically update the score.

--- survivors_game.gd
+++ survivors_game.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+var score = 0
+
 func spawn_mob():
-	var new_mob = preload("res://mob.tscn").instantiate()
+	var mob = preload("res://mob.tscn").instantiate()
 	%PathFollow2D.progress_ratio = randf()
-	new_mob.global_position = %PathFollow2D.global_position
-	add_child(new_mob)
+	mob.global_position = %PathFollow2D.global_position
+	mob.death.connect(_on_mob_death)
+	add_child(mob)
 
 
 func _on_timer_timeout():
@@ -14,3 +17,8 @@ func _on_timer_timeout():
 func _on_player_death():
 	%GameOver.visible = true
 	get_tree().paused = true
+	
+func _on_mob_death():
+	score += 1
+	var label = "Score: %s"
+	%Score.text = label % score
